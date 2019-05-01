9cfa646ed214b5aee5779f0f93148360a312bd5e
5j9 <5j9@users.noreply.github.com>
2019-05-01 22:54:43 +0430

    install_requires=[requests, beautifulsoup4], python_requires='>=3.6'

--- setup.py
+++ setup.py
@@ -8,4 +8,6 @@ setup(name='dlink2750u',
       author_email='5j9@users.noreply.github.com',
       license='GPLv3',
       packages=['dlink2750u'],
+      install_requires=['requests', 'beautifulsoup4'],
+      python_requires='>=3.6',
       zip_safe=True)
