030d6c2949ae1b82dcaeec55569dec3492a39e88
5j9 <5j9@users.noreply.github.com>
2019-05-01 20:53:37 +0430

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dlink2750u',
+      version='0.1',
+      description='A library to communicate with DLink2750U model routers',
+      url='https://github.com/5j9/dlink2750u',
+      author='5j9',
+      author_email='5j9@users.noreply.github.com',
+      license='GPLv3',
+      packages=['dlink2750u'],
+      zip_safe=True)
