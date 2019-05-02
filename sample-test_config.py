36e3dd1cc4d2e93f7ba5b75428b771a9bbd73a84
5j9 <5j9@users.noreply.github.com>
2019-05-02 13:53:01 +0430

    Rename sample-test_config.py to test_config-sample.py

--- sample-test_config.py
+++
@@ -1,3 +0,0 @@
-# Rename this file test_config.py and adjust the values
-ip_address = '192.168.1.1'
-auth = ('admin', 'admin')
---
+++ test_config-sample.py
@@ -0,0 +1,3 @@
+# Rename this file to test_config.py and adjust the values for testing
+ip_address = '192.168.1.1'
+auth = ('admin', 'admin')
